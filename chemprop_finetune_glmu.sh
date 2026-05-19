03f86cb93699a746eebd5e40997aeb3f4665e16f
Arunkumar Anthony <aa20.arunantony@gmail.com>
2026-05-19 13:07:32 +0530

    glmu fine tune no freeze encoder

--- chemprop_finetune_glmu.sh
+++ chemprop_finetune_glmu.sh
@@ -9,12 +9,11 @@ do
     --target-columns activity \
     --molecule-featurizers v1_rdkit_2d_normalized \
     --checkpoint models/finetune_v6_rdkit_scaffold/replicate_${i}/model_0/best.pt \
-    --save-dir models/glmu_transfer_ensemble_freeze/replicate_${i} \
+    --save-dir models/glmu_transfer_ensemble_no_freeze/replicate_${i} \
     --epochs 10 \
     --init-lr 1e-5 \
     --metrics roc prc f1 binary-mcc \
     --class-balance \
     --split scaffold_balanced \
-    --freeze-encoder \
     --num-replicates 1
 done
